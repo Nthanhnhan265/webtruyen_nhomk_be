68a6c452151ab49eadab9d46fa761647fef00b7b
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-17 14:59:06 +0700

    user api

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path')
 const express = require('express'); 
 const config = require('./config/config');
 const app = express(); 
+const api = require('./routes/api.js'); 
 const PORT = process.env.PORT
 // configuration: static files, json() and urlencoded()
 config(app,express); 
-
+app.use('/user',api)
 app.listen(PORT || 3001,()=>{
     console.log(`server is running on ${PORT}`); 
 })
