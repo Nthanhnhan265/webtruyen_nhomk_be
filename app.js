7d7d1508510faa7c8a67d4b06ea6cdcbd6f7a3a0
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-18 15:13:16 +0700

    update: create user

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //Require
 require('dotenv').config()
 require('module-alias/register')
+
+const createError = require('http-errors')
+const message = require('@root/message.js')
 const cors = require('cors')
 const path = require('path')
 const express = require('express')
 const config = require('./src/config/config.js')
 const app = express()
 const userRouter = require('./src/routes/user.router.js')
-const { log } = require('console')
 const PORT = process.env.PORT
 // const sequelize = require('./src/config/db_config.js')
 // const usermodel = require('./src/models/user.model.js')
@@ -18,6 +20,20 @@ app.use(cors({ credentials: true, origin: true }))
 
 //middleware & router
 app.use('/api/users', userRouter)
+
+//Middleware: error handler
+app.use((req, res, next) => {
+  next(createError(404, message.generalErrors.notFound))
+})
+
+// Middleware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    status: err.status || 500,
+    message: err.message || message.generalErrors.serverError,
+  })
+})
 
 app.listen(PORT || 3001, () => {
   console.log(`server is running on ${PORT}`)
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,28 +1,102 @@
-const { getAllUsers, getUserByID } = require('@services/user.service')
+const createHttpError = require('http-errors')
+const {
+  getAllUsers,
+  getUserByID,
+  createUser,
+} = require('@services/user.service')
+const message = require('@root/message')
 
+// ==========================
+// User Handler Functions
+// ==========================
+// CREATE USER
+/**
+ * Xử lý yêu cầu tạo một người dùng mới.
+ * @param {Object} req - Đối tượng yêu cầu.
+ * @param {Object} res - Đối tượng phản hồi.
+ * @param {Function} next - Hàm gọi tiếp theo trong middleware.
+ */
+async function handleCreateUser(req, res, next) {
+  //lấy dữ liệu từ form
+  const {
+    username,
+    email,
+    password,
+    confirmPassword,
+    role_id,
+    status,
+    avatar,
+  } = req.body
+
+  //xác thực đầu vào
+
+  //tạo người dùng mới
+  try {
+    const newUser = await createUser({
+      username,
+      email,
+      password,
+      role_id,
+      status,
+      avatar,
+    })
+
+    return res.status(201).json({
+      success: true,
+      status: 201,
+      message: message.user.createSucess,
+      data: newUser,
+      links: [],
+    })
+  } catch (error) {
+    // console.log(error)
+    next(error)
+  }
+}
+
+// READ USER
+/**
+ * Xử lý yêu cầu lấy danh sách tất cả người dùng.
+ * @param {Object} req - Đối tượng yêu cầu.
+ * @param {Object} res - Đối tượng phản hồi.
+ * @param {Function} next - Hàm gọi tiếp theo trong middleware.
+ * @returns {Promise<void>} - Không trả về giá trị.
+ */
 async function handleGetUser(req, res, next) {
-  let data = []
-  data = await getAllUsers()
+  let data = (data = await getAllUsers())
   return res.status(200).json({
     success: true,
-    message: 'user retrieved successfully',
+    message: message.user.fetchSucess,
     data: data,
   })
 }
 
+/**
+ * Xử lý yêu cầu lấy thông tin chi tiết của người dùng theo ID.
+ * @param {Object} req - Đối tượng yêu cầu.
+ * @param {Object} res - Đối tượng phản hồi.
+ * @param {Function} next - Hàm gọi tiếp theo trong middleware.
+ * @returns {Promise<void>} - Không trả về giá trị.
+ */
 async function handleGetUserByID(req, res, next) {
   const id = req.params.id
-  console.log('>>>id: ' + id)
-  let data = []
-  data = await getUserByID(id)
+  let data = await getUserByID(id)
+
+  // Kiểm tra xem người dùng có tồn tại không
+  if (!data) {
+    next(createHttpError(404, message.user.notFound))
+  }
+
   return res.status(200).json({
     success: true,
-    message: 'user retrieved successfully',
+    message: message.user.fetchSucess,
     data: data,
+    links: [],
   })
 }
 
 module.exports = {
   handleGetUser,
   handleGetUserByID,
+  handleCreateUser,
 }
