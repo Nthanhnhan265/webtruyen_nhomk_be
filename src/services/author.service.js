37c28f4116157b9d08afabcfda04122dafaa275c
vo02022004 <148637808+vo02022004@users.noreply.github.com>
2024-10-22 05:44:46 +0700

    cập nhật chức năng tìm kiếm và phân trang

--- src/services/author.service.js
+++ src/services/author.service.js
@@ -1,23 +1,50 @@
 const Author = require('../models/author.model');
+const { Op } = require('sequelize'); // Import toán tử Op từ Sequelize
 
 // Tạo tác giả mới
 const createAuthor = async (authorData) => {
     return await Author.create(authorData);
 };
 
-// Lấy tất cả tác giả
-const getAllAuthors = async (sortOrder) => {
+
+const getAllAuthors = async (author_name, description, sortOrder, page, limit) => {
+    console.log("check author_name", author_name);
+    console.log("check description", description);
+    console.log("check sort", sortOrder);
+    console.log("check page", page);
+    console.log("check limit", limit);
+
     try {
+        const whereConditions = {};
+
+        if (author_name) {
+            whereConditions.author_name = {
+                [Op.like]: `%${author_name}%`
+            };
+        }
+
+        if (description) {
+            whereConditions.description = {
+                [Op.like]: `%${description}%`
+            };
+        }
+        console.log(whereConditions);
+
         const authors = await Author.findAll({
+            where: {
+                [Op.or]: whereConditions,// Use Op.or to search for either condition
+            },
             order: [
-                // ['author_name', sortOrder === 'desc' ? 'DESC' : 'ASC'], // Sort by author_name
-                ['author_name', sortOrder === 'desc' ? 'ASC' : 'DESC'], // Sort by author_name
+                ['author_name', sortOrder === 'desc' ? 'DESC' : 'ASC'],
             ],
+            offset: (page - 1) * limit, // Tính toán offset cho phân trang
+            limit: limit, // Giới hạn số lượng hàng
         });
+
         return authors;
     } catch (error) {
         console.error('Error fetching authors:', error);
-        throw error; // Handle the error appropriately
+        throw error;
     }
 };
 
